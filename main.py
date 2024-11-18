95dc5e113525ab7281d53b760109f222c394c408
donaldnguyen99 <donaldnguyen99@users.noreply.github.com>
2024-11-18 03:00:35 -0800

    add moveable player

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import pygame
 
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return 
+        
+        screen.fill(pygame.Color("black"))
+        player.draw(screen)
+        player.update(dt)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
