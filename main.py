132341c99ff8e1914f5ec25b03c94ef9d87f2008
donaldnguyen99 <donaldnguyen99@users.noreply.github.com>
2024-11-21 01:14:19 -0800

    add shooting ability

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from player import Player
+from shot import Shot
 
 def main():
     pygame.init()
@@ -11,8 +14,17 @@ def main():
 
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
     Player.containers = (updatables, drawables)
+    Shot.containers = (shots, updatables, drawables)
+
+    asteroidfield = AsteroidField() 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     running = True
     while running:
@@ -24,6 +36,15 @@ def main():
         screen.fill(pygame.Color("black"))
         for updatable in updatables:
             updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                pygame.quit()
+                return
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    asteroid.splitd()
+                    shot.kill()
         for drawable in drawables:
             drawable.draw(screen)
 
